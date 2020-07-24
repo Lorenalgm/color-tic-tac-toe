fdec280ea2597354efb20122011bb9548d6d02c6
MANN03U <emmanuel_costasilva@hotmail.com>
2020-07-24 01:11:55 -0300

    refactor

--- script.js
+++ script.js
@@ -1,102 +1,37 @@
-var card1 = document.querySelector(".card1");
-var card2 = document.querySelector(".card2");
-var card3 = document.querySelector(".card3");
-var card4 = document.querySelector(".card4");
-var card5 = document.querySelector(".card5");
-var card6 = document.querySelector(".card6");
-var card7 = document.querySelector(".card7");
-var card8 = document.querySelector(".card8");
-var card9 = document.querySelector(".card9");
+//Pegando todos os cards em um array
+const cards = document.querySelectorAll(".card");
 
 let jogador = true;
 
-card1.addEventListener("click", function(){
-    if(jogador){
-        card1.style.background = 'red';
-        jogador=false;
-    }else{
-        card1.style.background = 'yellow';
-        jogador=true;
+//Percorrendo o array de cards e para cada card adicionar um event listen
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    if (jogador) {
+      //Verificando se um square está sem a classe target para que o player possa marcar
+      //Caso esteja com target, não irá marcar
+      if (!card.classList.contains("target")) {
+        card.style.backgroundColor = "red";
+
+        //Adicionando uma classe para os quadrados que já estão marcados por algum player
+        card.classList.add("target");
+
+        //O jogador será o valor inverso ao atual
+        //Se player1, então player2. Se player2, então player1
+        jogador = !jogador;
+      }
+    } else {
+      //Verificando se um square está sem a classe target para que o player possa marcar
+      //Caso esteja com target, não irá marcar
+      if (!card.classList.contains("target")) {
+        card.style.backgroundColor = "yellow";
+
+        //Adicionando uma classe para os quadrados que já estão marcados por algum player
+        card.classList.add("target");
+
+        //O jogador será o valor inverso ao atual
+        //Se player1, então player2. Se player2, então player1
+        jogador = !jogador;
+      }
     }
+  });
 });
-
- card2.addEventListener("click", function(){
-    if(jogador){
-        card2.style.background = 'red';
-        jogador=false;
-    }else{
-        card2.style.background = 'yellow';
-        jogador=true;
-    }
- });
-
- card3.addEventListener("click", function(){
-    if(jogador){
-        card3.style.background = 'red';
-        jogador=false;
-    }else{
-        card3.style.background = 'yellow';
-        jogador=true;
-    }
- });
-
- card4.addEventListener("click", function(){
-    if(jogador){
-        card4.style.background = 'red';
-        jogador=false;
-    }else{
-        card4.style.background = 'yellow';
-        jogador=true;
-    }
- });
-
- card5.addEventListener("click", function(){
-    if(jogador){
-        card5.style.background = 'red';
-        jogador=false;
-    }else{
-        card5.style.background = 'yellow';
-        jogador=true;
-    }
- });
-
- card6.addEventListener("click", function(){
-    if(jogador){
-        card6.style.background = 'red';
-        jogador=false;
-    }else{
-        card6.style.background = 'yellow';
-        jogador=true;
-    }
- });
-
- card7.addEventListener("click", function(){
-    if(jogador){
-        card7.style.background = 'red';
-        jogador=false;
-    }else{
-        card7.style.background = 'yellow';
-        jogador=true;
-    }
- });
- 
- card8.addEventListener("click", function(){
-    if(jogador){
-        card8.style.background = 'red';
-        jogador=false;
-    }else{
-        card8.style.background = 'yellow';
-        jogador=true;
-    }
- });
- 
- card9.addEventListener("click", function(){
-    if(jogador){
-        card9.style.background = 'red';
-        jogador=false;
-    }else{
-        card9.style.background = 'yellow';
-        jogador=true;
-    }
- });
- 
