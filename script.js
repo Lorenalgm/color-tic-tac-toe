8bff44b06728eb096a0841a577f7561e1ccfa3d5
Lorenalgm <lorena@proesc.com>
2020-07-23 23:16:37 -0300

    Adicionado javascript

---
+++ script.js
@@ -0,0 +1,102 @@
+var card1 = document.querySelector(".card1");
+var card2 = document.querySelector(".card2");
+var card3 = document.querySelector(".card3");
+var card4 = document.querySelector(".card4");
+var card5 = document.querySelector(".card5");
+var card6 = document.querySelector(".card6");
+var card7 = document.querySelector(".card7");
+var card8 = document.querySelector(".card8");
+var card9 = document.querySelector(".card9");
+
+let jogador = true;
+
+card1.addEventListener("click", function(){
+    if(jogador){
+        card1.style.background = 'red';
+        jogador=false;
+    }else{
+        card1.style.background = 'yellow';
+        jogador=true;
+    }
+});
+
+ card2.addEventListener("click", function(){
+    if(jogador){
+        card2.style.background = 'red';
+        jogador=false;
+    }else{
+        card2.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+
+ card3.addEventListener("click", function(){
+    if(jogador){
+        card3.style.background = 'red';
+        jogador=false;
+    }else{
+        card3.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+
+ card4.addEventListener("click", function(){
+    if(jogador){
+        card4.style.background = 'red';
+        jogador=false;
+    }else{
+        card4.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+
+ card5.addEventListener("click", function(){
+    if(jogador){
+        card5.style.background = 'red';
+        jogador=false;
+    }else{
+        card5.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+
+ card6.addEventListener("click", function(){
+    if(jogador){
+        card6.style.background = 'red';
+        jogador=false;
+    }else{
+        card6.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+
+ card7.addEventListener("click", function(){
+    if(jogador){
+        card7.style.background = 'red';
+        jogador=false;
+    }else{
+        card7.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+ 
+ card8.addEventListener("click", function(){
+    if(jogador){
+        card8.style.background = 'red';
+        jogador=false;
+    }else{
+        card8.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+ 
+ card9.addEventListener("click", function(){
+    if(jogador){
+        card9.style.background = 'red';
+        jogador=false;
+    }else{
+        card9.style.background = 'yellow';
+        jogador=true;
+    }
+ });
+ 
